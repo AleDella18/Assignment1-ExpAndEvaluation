a3632d3a31633feb142f7be6e5555865fa084c7e
Stipe Peran <perans@usi.ch>
2024-10-27 11:09:22 +0100

    Created a new Sorter interface for rewriting the entire code because the previuos one was messed up. Now we can generate all tests for class Integer and boc plots as well

--- Code/Sorter.java
+++ Code/Sorter.java
@@ -2,15 +2,14 @@ package Code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-
+import java.io.*;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public interface Sorter<T extends Comparable<T>> {
-
     void sort(T[] items);
 
-    public static void exportToCSV(long[] times, String filename) {
+    static void exportToCSV(long[] times, String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
             for (long time : times) {
                 writer.write(time + ",");
@@ -23,21 +22,22 @@ public interface Sorter<T extends Comparable<T>> {
 
     static void main(String[] args) {
 
-        long countBSUNC = 0;
-        long countBSWN = 0;
-        long countQSGPT = 0;
-        long countSSGPT = 0;
-
         int iterations = 41;
 
-        // Assertion for having an array of execution times when we can access directly
-        // minimum, first quartile, median, third quartile, and maximum
-        assert iterations % 4 == 1;
+        long[] executionTimesSmallArrays1 = new long[iterations];
+        long[] executionTimesSmallArrays2 = new long[iterations];
+        long[] executionTimesSmallArrays3 = new long[iterations];
+        long[] executionTimesSmallArrays4 = new long[iterations];
 
-        long[] arrayIntegers1 = new long[iterations];
-        long[] arrayIntegers2 = new long[iterations];
-        long[] arrayIntegers3 = new long[iterations];
-        long[] arrayIntegers4 = new long[iterations];
+        long[] executionTimesMediumArrays1 = new long[iterations];
+        long[] executionTimesMediumArrays2 = new long[iterations];
+        long[] executionTimesMediumArrays3 = new long[iterations];
+        long[] executionTimesMediumArrays4 = new long[iterations];
+
+        long[] executionTimesLargeArrays1 = new long[iterations];
+        long[] executionTimesLargeArrays2 = new long[iterations];
+        long[] executionTimesLargeArrays3 = new long[iterations];
+        long[] executionTimesLargeArrays4 = new long[iterations];
 
         int smallSize = 100;
         int mediumSize = 10000;
@@ -47,14 +47,21 @@ public interface Sorter<T extends Comparable<T>> {
         for (int i = 0; i < smallSize; ++i) {
             testIntegersSmallArraySorted[i] = i;
         }
+
         Integer[] testIntegersSmallArrayReverseSorted = new Integer[smallSize];
         for (int i = 0; i < smallSize; ++i) {
             testIntegersSmallArrayReverseSorted[i] = smallSize - 1 - i;
         }
+
         Integer[] testIntegersSmallArrayRandom = new Integer[smallSize];
         for (int i = 0; i < smallSize; ++i) {
             testIntegersSmallArrayRandom[i] = (int)(Math.random() * smallSize);
         }
+
+        List<Integer[]> arrayListIntegerSmallArraysTests = new ArrayList<>();
+        arrayListIntegerSmallArraysTests.add(testIntegersSmallArraySorted);
+        arrayListIntegerSmallArraysTests.add(testIntegersSmallArrayReverseSorted);
+        arrayListIntegerSmallArraysTests.add(testIntegersSmallArrayRandom);
 
         Integer[] testIntegersMediumArraySorted = new Integer[mediumSize];
         for (int i = 0; i < mediumSize; ++i) {
@@ -71,6 +78,11 @@ public interface Sorter<T extends Comparable<T>> {
             testIntegersMediumArrayRandom[i] = (int)(Math.random() * mediumSize);
         }
 
+        List<Integer[]> arrayListIntegerMediumArraysTests = new ArrayList<>();
+        arrayListIntegerMediumArraysTests.add(testIntegersMediumArraySorted);
+        arrayListIntegerMediumArraysTests.add(testIntegersMediumArrayReverseSorted);
+        arrayListIntegerMediumArraysTests.add(testIntegersMediumArrayRandom);
+
         Integer[] testIntegersLargeArraySorted = new Integer[largeSize];
         for (int i = 0; i < largeSize; ++i) {
             testIntegersLargeArraySorted[i] = i;
@@ -86,48 +98,10 @@ public interface Sorter<T extends Comparable<T>> {
             testIntegersLargeArrayRandom[i] = (int)(Math.random() * largeSize);
         }
 
-        List<Integer[]> arrayListIntegers = new ArrayList<>();
-        arrayListIntegers.add(testIntegersSmallArraySorted);
-        arrayListIntegers.add(testIntegersSmallArrayReverseSorted);
-        arrayListIntegers.add(testIntegersSmallArrayRandom);
-        /*arrayListIntegers.add(testIntegersMediumArraySorted);
-        arrayListIntegers.add(testIntegersMediumArrayReverseSorted);
-        arrayListIntegers.add(testIntegersMediumArrayRandom);
-        arrayListIntegers.add(testIntegersLargeArraySorted);
-        arrayListIntegers.add(testIntegersLargeArrayReverseSorted);
-        arrayListIntegers.add(testIntegersLargeArrayRandom);*/
-
-        long[] arrayStrings1 = new long[iterations];
-        long[] arrayStrings2 = new long[iterations];
-        long[] arrayStrings3 = new long[iterations];
-        long[] arrayStrings4 = new long[iterations];
-
-        String[] testStrings1 = {"apple", "banana", "cherry", "date"};
-        String[] testStrings2 = {"zebra", "yak", "xerox", "walrus"};
-        String[] testStrings3 = {"apple", "banana", "banana", "cherry", "apple"};
-        String[] testStrings4 = {"test", "test", "test", "test"};
-        String[] testStrings5 = {"", "banana", "", "cherry", "apple"};
-        String[] testStrings6 = {"banana", "apple", "cherry", "date"};
-        String[] testStrings7 = {"Apple", "banana", "Cherry", "apple", "Banana"};
-        String[] testStrings8 = {"Hello"};
-
-		/*String[] testStrings8 = new String[1000]; // Fill with random values or sequential numbers
-		for (int i = 0; i < testStrings8.length; i++) {
-			testStrings8[i] = (int)(Math.random() * 1000);
-		}*/
-
-        List<String[]> arrayListStrings = new ArrayList<>();
-        arrayListStrings.add(testStrings1);
-        arrayListStrings.add(testStrings2);
-        arrayListStrings.add(testStrings3);
-        arrayListStrings.add(testStrings4);
-        arrayListStrings.add(testStrings5);
-        arrayListStrings.add(testStrings6);
-        arrayListStrings.add(testStrings7);
-        arrayListStrings.add(testStrings8);
-
-        System.out.println();
-        // Accessing and printing each array
+        List<Integer[]> arrayListIntegerLargeArraysTests = new ArrayList<>();
+        arrayListIntegerLargeArraysTests.add(testIntegersLargeArraySorted);
+        arrayListIntegerLargeArraysTests.add(testIntegersLargeArrayReverseSorted);
+        arrayListIntegerLargeArraysTests.add(testIntegersLargeArrayRandom);
 
         // Instances of the 4 algorithms
         BubbleSortUntilNoChange<Integer> BSUNCIntegers = new BubbleSortUntilNoChange<>();
@@ -139,17 +113,7 @@ public interface Sorter<T extends Comparable<T>> {
         long endTime;
         long totalTime;
 
-        for (Integer[] array : arrayListIntegers) {
-            // Iterations
-            // Print the formatted array
-            System.out.print("Analysis for the array {");
-            for (int i = 0; i < array.length; i++) {
-                System.out.print(array[i]);
-                if (i < array.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println("}:");
+        for (Integer[] array : arrayListIntegerSmallArraysTests) {
             for (int i = 0; i < iterations; ++i) {
 
                 startTime = System.nanoTime();
@@ -158,7 +122,7 @@ public interface Sorter<T extends Comparable<T>> {
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayIntegers1[i] = totalTime;
+                executionTimesSmallArrays1[i] = totalTime;
 
                 startTime = System.nanoTime();
                 BSWNIntegers.sort(array);
@@ -166,7 +130,7 @@ public interface Sorter<T extends Comparable<T>> {
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayIntegers2[i] = totalTime;
+                executionTimesSmallArrays2[i] = totalTime;
 
                 startTime = System.nanoTime();
                 QSGPTIntegers.sort(array);
@@ -174,7 +138,7 @@ public interface Sorter<T extends Comparable<T>> {
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayIntegers3[i] = totalTime;
+                executionTimesSmallArrays3[i] = totalTime;
 
                 startTime = System.nanoTime();
                 SSGPTIntegers.sort(array);
@@ -182,140 +146,113 @@ public interface Sorter<T extends Comparable<T>> {
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayIntegers4[i] = totalTime;
-
-                System.out.print("Iteration " + (i + 1) + ": Total time for BSUNC: " + arrayIntegers1[i]);
-                System.out.print(" Total time for BSWN : " + arrayIntegers2[i]);
-                System.out.print(" Total time for QSGPT : " + arrayIntegers3[i]);
-                System.out.println(" Total time for SSGPT : " + arrayIntegers4[i]);
+                executionTimesSmallArrays4[i] = totalTime;
             }
 
-            Arrays.sort(arrayIntegers1);
-            Arrays.sort(arrayIntegers2);
-            Arrays.sort(arrayIntegers3);
-            Arrays.sort(arrayIntegers4);
-
-            long min = Math.min(Math.min(arrayIntegers1[iterations/2], arrayIntegers2[iterations/2]), Math.min(arrayIntegers3[iterations/2], arrayIntegers4[iterations/2]));
-            if (min == arrayIntegers1[iterations/2]) {
-                ++countBSUNC;
-            }
-            else if (min == arrayIntegers2[iterations/2]) {
-                ++countBSWN;
-            }
-            else if (min == arrayIntegers3[iterations/2]) {
-                ++countQSGPT;
-            }
-            else {
-                ++countSSGPT;
-            }
-            System.out.println("Minimum time for BSUNC : " + arrayIntegers1[0] + " First quartile time for BSUNC : " + arrayIntegers1[iterations/4] + " Median time for BSUNC : " + arrayIntegers1[iterations/2] + " Third quartile time for BSUNC : " + arrayIntegers1[3 * iterations / 4] + " Maximum time for BSUNC : " + arrayIntegers1[iterations - 1]);
-            System.out.println("Minimum time for BSWN : " + arrayIntegers2[0] + " First quartile time for BSWN : " + arrayIntegers2[iterations/4] + " Median time for BSWN : " + arrayIntegers2[iterations/2] + " Third quartile time for BSWN : " + arrayIntegers2[3 * iterations / 4] + " Maximum time for BSWN : " + arrayIntegers2[iterations - 1]);
-            System.out.println("Minimum time for QSGPT : " + arrayIntegers3[0] + " First quartile time for QSGPT : " + arrayIntegers3[iterations/4] + " Median time for QSGPT : " + arrayIntegers3[iterations/2] + " Third quartile time for QSGPT : " + arrayIntegers3[3 * iterations / 4] + " Maximum time for QSGPT : " + arrayIntegers3[iterations - 1]);
-            System.out.println("Minimum time for SSGPT : " + arrayIntegers4[0] + " First quartile time for SSGPT : " + arrayIntegers4[iterations/4] + " Median time for SSGPT : " + arrayIntegers4[iterations/2] + " Third quartile time for SSGPT : " + arrayIntegers4[3 * iterations / 4] + " Maximum time for SSGPT : " + arrayIntegers4[iterations - 1]);
-            System.out.println();
+            Arrays.sort(executionTimesSmallArrays1);
+            Arrays.sort(executionTimesSmallArrays2);
+            Arrays.sort(executionTimesSmallArrays3);
+            Arrays.sort(executionTimesSmallArrays4);
         }
 
-        // After measuring times, export them to CSV
-        exportToCSV(arrayIntegers1, "BubbleSortUntilNoChange_Times.csv");
-        exportToCSV(arrayIntegers2, "BubbleSortWhileNeeded_Times.csv");
-        exportToCSV(arrayIntegers3, "QuickSortGPT_Times.csv");
-        exportToCSV(arrayIntegers4, "SelectionSortGPT_Times.csv");
+        exportToCSV(executionTimesSmallArrays1, "BubbleSortUntilNoChange_Integer_SmallArray_Times.csv");
+        exportToCSV(executionTimesSmallArrays2, "BubbleSortWhileNeeded_Integer_SmallArray_Times.csv");
+        exportToCSV(executionTimesSmallArrays3, "QuickSortGPT_Integer_SmallArray_Times.csv");
+        exportToCSV(executionTimesSmallArrays4, "SelectionSortGPT_Integer_SmallArray_Times.csv");
 
-        BubbleSortUntilNoChange<String> BSUNCStrings = new BubbleSortUntilNoChange<>();
-        BubbleSortWhileNeeded<String> BSWNStrings = new BubbleSortWhileNeeded<>();
-        QuickSortGPT<String> QSGPTStrings = new QuickSortGPT<>();
-        SelectionSortGPT<String> SSGPTStrings = new SelectionSortGPT<>();
-
-        for (String[] array : arrayListStrings) {
-            // Iterations
-            // Print the formatted array
-            System.out.print("Analysis for the array {");
-            for (int i = 0; i < array.length; i++) {
-                System.out.print(array[i]);
-                if (i < array.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println("}:");
+        for (Integer[] array : arrayListIntegerMediumArraysTests) {
             for (int i = 0; i < iterations; ++i) {
 
                 startTime = System.nanoTime();
-                BSUNCStrings.sort(array);
+                BSUNCIntegers.sort(array);
                 endTime = System.nanoTime();
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayStrings1[i] = totalTime;
+                executionTimesMediumArrays1[i] = totalTime;
 
                 startTime = System.nanoTime();
-                BSWNStrings.sort(array);
+                BSWNIntegers.sort(array);
                 endTime = System.nanoTime();
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayStrings2[i] = totalTime;
+                executionTimesMediumArrays2[i] = totalTime;
 
                 startTime = System.nanoTime();
-                QSGPTStrings.sort(array);
+                QSGPTIntegers.sort(array);
                 endTime = System.nanoTime();
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayStrings3[i] = totalTime;
+                executionTimesMediumArrays3[i] = totalTime;
 
                 startTime = System.nanoTime();
-                SSGPTStrings.sort(array);
+                SSGPTIntegers.sort(array);
                 endTime = System.nanoTime();
 
                 // Calculate the total time taken
                 totalTime = endTime - startTime;
-                arrayStrings4[i] = totalTime;
-
-                System.out.print("Iteration " + (i + 1) + ": Total time for BSUNC: " + arrayStrings1[i]);
-                System.out.print(" Total time for BSWN : " + arrayStrings2[i]);
-                System.out.print(" Total time for QSGPT : " + arrayStrings3[i]);
-                System.out.println(" Total time for SSGPT : " + arrayStrings4[i]);
+                executionTimesMediumArrays4[i] = totalTime;
             }
 
-            Arrays.sort(arrayStrings1);
-            Arrays.sort(arrayStrings2);
-            Arrays.sort(arrayStrings3);
-            Arrays.sort(arrayStrings4);
-
-            long min = Math.min(Math.min(arrayStrings1[iterations/2], arrayStrings2[iterations/2]), Math.min(arrayStrings3[iterations/2], arrayStrings4[iterations/2]));
-            if (min == arrayStrings1[iterations/2]) {
-                ++countBSUNC;
-            }
-            else if (min == arrayStrings2[iterations/2]) {
-                ++countBSWN;
-            }
-            else if (min == arrayStrings3[iterations/2]) {
-                ++countQSGPT;
-            }
-            else {
-                ++countSSGPT;
-            }
-            System.out.print("Median time for BSUNC : " + arrayStrings1[iterations/2]);
-            System.out.print(" Median time for BSWN : " + arrayStrings2[iterations/2]);
-            System.out.print(" Median time for QSGPT : " + arrayStrings3[iterations/2]);
-            System.out.println(" Median time for SSGPT : " + arrayStrings4[iterations/2]);
-            System.out.println();
+            Arrays.sort(executionTimesMediumArrays1);
+            Arrays.sort(executionTimesMediumArrays2);
+            Arrays.sort(executionTimesMediumArrays3);
+            Arrays.sort(executionTimesMediumArrays4);
         }
 
-        System.out.print("The fastest algorithm is : ");
-        long max_count = Math.max(Math.max(countBSUNC, countBSWN), Math.max(countQSGPT, countSSGPT));
-        if (max_count == countBSUNC) {
-            System.out.println("BSUNC");
+        exportToCSV(executionTimesMediumArrays1, "BubbleSortUntilNoChange_Integer_MediumArray_Times.csv");
+        exportToCSV(executionTimesMediumArrays2, "BubbleSortWhileNeeded_Integer_MediumArray_Times.csv");
+        exportToCSV(executionTimesMediumArrays3, "QuickSortGPT_Integer_MediumArray_Times.csv");
+        exportToCSV(executionTimesMediumArrays4, "SelectionSortGPT_Integer_MediumArray_Times.csv");
+
+        for (Integer[] array : arrayListIntegerLargeArraysTests) {
+            for (int i = 0; i < iterations; ++i) {
+
+                startTime = System.nanoTime();
+                BSUNCIntegers.sort(array);
+                endTime = System.nanoTime();
+
+                // Calculate the total time taken
+                totalTime = endTime - startTime;
+                executionTimesLargeArrays1[i] = totalTime;
+
+                startTime = System.nanoTime();
+                BSWNIntegers.sort(array);
+                endTime = System.nanoTime();
+
+                // Calculate the total time taken
+                totalTime = endTime - startTime;
+                executionTimesLargeArrays2[i] = totalTime;
+
+                startTime = System.nanoTime();
+                QSGPTIntegers.sort(array);
+                endTime = System.nanoTime();
+
+                // Calculate the total time taken
+                totalTime = endTime - startTime;
+                executionTimesLargeArrays3[i] = totalTime;
+
+                startTime = System.nanoTime();
+                SSGPTIntegers.sort(array);
+                endTime = System.nanoTime();
+
+                // Calculate the total time taken
+                totalTime = endTime - startTime;
+                executionTimesLargeArrays4[i] = totalTime;
+            }
+
+            Arrays.sort(executionTimesLargeArrays1);
+            Arrays.sort(executionTimesLargeArrays2);
+            Arrays.sort(executionTimesLargeArrays3);
+            Arrays.sort(executionTimesLargeArrays4);
         }
-        else if (max_count == countBSWN) {
-            System.out.println("BSWN");
-        }
-        else if (max_count == countQSGPT) {
-            System.out.println("QSGPT");
-        }
-        else {
-            System.out.println("SSGPT");
-        }
+
+        exportToCSV(executionTimesLargeArrays1, "BubbleSortUntilNoChange_Integer_LargeArray_Times.csv");
+        exportToCSV(executionTimesLargeArrays2, "BubbleSortWhileNeeded_Integer_LargeArray_Times.csv");
+        exportToCSV(executionTimesLargeArrays3, "QuickSortGPT_Integer_LargeArray_Times.csv");
+        exportToCSV(executionTimesLargeArrays4, "SelectionSortGPT_Integer_LargeArray_Times.csv");
 
     }
 }
--- boxplot.py
+++ boxplot.py
@@ -3,10 +3,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the CSV files
-bubble_unc_times = pd.read_csv("BubbleSortUntilNoChange_Times.csv", header=None).T
-bubble_needed_times = pd.read_csv("BubbleSortWhileNeeded_Times.csv", header=None).T
-quick_times = pd.read_csv("QuickSortGPT_Times.csv", header=None).T
-selection_times = pd.read_csv("SelectionSortGPT_Times.csv", header=None).T
+bubble_unc_times = pd.read_csv("BubbleSortUntilNoChange_Integer_SmallArray_Times.csv", header=None).T
+bubble_needed_times = pd.read_csv("BubbleSortWhileNeeded_Integer_SmallArray_Times.csv", header=None).T
+quick_times = pd.read_csv("QuickSortGPT_Integer_SmallArray_Times.csv", header=None).T
+selection_times = pd.read_csv("SelectionSortGPT_Integer_SmallArray_Times.csv", header=None).T
 
 # Combine into a single DataFrame
 bubble_unc_times['Algorithm'] = 'BubbleSortUntilNoChange'
@@ -21,5 +21,61 @@ df = df.melt(id_vars=['Algorithm'], var_name='Iteration', value_name='Time (ns)'
 plt.figure(figsize=(12, 8))
 sns.boxplot(data=df, x='Algorithm', y='Time (ns)')
 plt.yscale("log")  # Log scale if needed
-plt.title("Execution Time of Sorting Algorithms")
+plt.title("Execution Time of Sorting Algorithms for small arrays")
+
+# Save the plot to a file
+plt.savefig("BoxPlot_Integer_Small_Arrays.png", format="png", dpi=300)
+
+plt.show()
+
+# Load the CSV files
+bubble_unc_times = pd.read_csv("BubbleSortUntilNoChange_Integer_MediumArray_Times.csv", header=None).T
+bubble_needed_times = pd.read_csv("BubbleSortWhileNeeded_Integer_MediumArray_Times.csv", header=None).T
+quick_times = pd.read_csv("QuickSortGPT_Integer_MediumArray_Times.csv", header=None).T
+selection_times = pd.read_csv("SelectionSortGPT_Integer_MediumArray_Times.csv", header=None).T
+
+# Combine into a single DataFrame
+bubble_unc_times['Algorithm'] = 'BubbleSortUntilNoChange'
+bubble_needed_times['Algorithm'] = 'BubbleSortWhileNeeded'
+quick_times['Algorithm'] = 'QuickSortGPT'
+selection_times['Algorithm'] = 'SelectionSortGPT'
+
+df = pd.concat([bubble_unc_times, bubble_needed_times, quick_times, selection_times], ignore_index=True)
+df = df.melt(id_vars=['Algorithm'], var_name='Iteration', value_name='Time (ns)')
+
+# Plot the boxplots
+plt.figure(figsize=(12, 8))
+sns.boxplot(data=df, x='Algorithm', y='Time (ns)')
+plt.yscale("log")  # Log scale if needed
+plt.title("Execution Time of Sorting Algorithms for medium arrays")
+
+# Save the plot to a file
+plt.savefig("BoxPlot_Integer_Medium_Arrays.png", format="png", dpi=300)
+
+plt.show()
+
+# Load the CSV files
+bubble_unc_times = pd.read_csv("BubbleSortUntilNoChange_Integer_LargeArray_Times.csv", header=None).T
+bubble_needed_times = pd.read_csv("BubbleSortWhileNeeded_Integer_LargeArray_Times.csv", header=None).T
+quick_times = pd.read_csv("QuickSortGPT_Integer_LargeArray_Times.csv", header=None).T
+selection_times = pd.read_csv("SelectionSortGPT_Integer_LargeArray_Times.csv", header=None).T
+
+# Combine into a single DataFrame
+bubble_unc_times['Algorithm'] = 'BubbleSortUntilNoChange'
+bubble_needed_times['Algorithm'] = 'BubbleSortWhileNeeded'
+quick_times['Algorithm'] = 'QuickSortGPT'
+selection_times['Algorithm'] = 'SelectionSortGPT'
+
+df = pd.concat([bubble_unc_times, bubble_needed_times, quick_times, selection_times], ignore_index=True)
+df = df.melt(id_vars=['Algorithm'], var_name='Iteration', value_name='Time (ns)')
+
+# Plot the boxplots
+plt.figure(figsize=(12, 8))
+sns.boxplot(data=df, x='Algorithm', y='Time (ns)')
+plt.yscale("log")  # Log scale if needed
+plt.title("Execution Time of Sorting Algorithms for large arrays")
+
+# Save the plot to a file
+plt.savefig("BoxPlot_Integer_Large_Arrays.png", format="png", dpi=300)
+
 plt.show()
